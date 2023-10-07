87f95afb4afc38494e3a1682b9c55e3c9054cae8
KatyaNichi <ekaterinanichiporenko@Ekaterinas-MacBook-Air.local>
2023-10-07 22:44:25 +0200

    added fetch to favorites

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -4,6 +4,7 @@ import Card from '../../components/common/Card/Card';
 import Header from '../../components/common/Header/Header';
 import { useRecipes } from '../../components/useRecipes';
 import { Recipe } from '../../../types/recipe';
+import { getBaseUrl } from '../../services/api';
 import './Favorite.css';
 
 const FavoritesPage = () => {
@@ -11,16 +12,45 @@ const FavoritesPage = () => {
   const { recipes: allRecipes } = useRecipes(); 
   const [favoriteRecipes, setFavoriteRecipes] = useState<Recipe[]>([]);
 
+  // useEffect(() => {
+  //   if (!userContext.isLoggedIn) {
+  //     return;
+  //   }
+  //   const favoriteRecipeIds = userContext.favorites;
+  //   const filteredRecipes = allRecipes.filter((recipe) =>
+  //     favoriteRecipeIds.includes(String(recipe.id))
+  //   );
+  //   setFavoriteRecipes(filteredRecipes);
+  // }, [userContext.isLoggedIn, userContext.favorites, allRecipes]);
+  
   useEffect(() => {
-    if (!userContext.isLoggedIn) {
-      return;
-    }
-    const favoriteRecipeIds = userContext.favorites;
-    const filteredRecipes = allRecipes.filter((recipe) =>
-      favoriteRecipeIds.includes(String(recipe.id))
-    );
-    setFavoriteRecipes(filteredRecipes);
-  }, [userContext.isLoggedIn, userContext.favorites, allRecipes]);
+    const fetchFavoriteRecipes = async () => {
+      if (userContext.isLoggedIn) {
+        try {
+          const response = await fetch(
+            getBaseUrl(`/api/favorites/user/${userContext.userId}`)
+          );
+
+          if (response.ok) {
+            const favoritesData = await response.json();
+            const favoriteRecipeIds = favoritesData.favorites;
+            const filteredRecipes = allRecipes.filter((recipe) =>
+              favoriteRecipeIds.includes(String(recipe.id))
+            );
+            setFavoriteRecipes(filteredRecipes);
+          } else {
+            console.error('Failed to fetch user favorites');
+          }
+        } catch (error) {
+          console.error('Error fetching user favorites:', error);
+        }
+      }
+    };
+
+    // Call the fetch function when the component mounts or when userContext.favorites changes
+    fetchFavoriteRecipes();
+  }, [userContext.isLoggedIn, userContext.userId, userContext.favorites, allRecipes]);
+
   if (!userContext.isLoggedIn) {
     return (
       <div className="favorites-page">
